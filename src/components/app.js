1eff322932240c4d18de1a1d270fca44a426abab
Destinee Muse <destineemuse20@yahoo.com>
2022-05-09 19:58:45 -0500

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+const greeting = 'Hello, Destinee';
+const loggedIn = true;
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>{loggedIn ? greeting : "Not logged in"}</h1>
+      </div>
+    );
+  }
+}
